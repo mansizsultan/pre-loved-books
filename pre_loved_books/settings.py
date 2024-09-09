efafa7d4f619252a6a48b754a2e48310464c1f8c
mansizsultan <sultanibnumansiz@gmail.com>
2024-09-10 06:12:59 +0700

    ADD url deployment

--- pre_loved_books/settings.py
+++ pre_loved_books/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-20*7caaz*-s1fu^-oyis2w^ae^$e&n1p^t+vheuoammq897tya
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sultan-ibnu-prelovebooks.pbp.cs.ui.ac.id"]
 
 
 # Application definition
